5f6ade4ae12adb7f9d5a6b74ad0a16bceda4fc64
Potwora <matthewpotwora@yahoo.com>
2022-05-21 21:57:53 -0700

    added alerts and tested for bugs

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,44 @@
 // Assignment code here
 function getRandomInt (max){
-  return Math.floor(math.random()*max);
+  return Math.floor(Math.random() * max);
 }
 var passwordOptions = {
   letters:"abcdefghijklmnopqrstuvwxyz",
   special:"`~!@#$%^&*()_-+={[}}|:;'<>,./?",
-  numbers:"123456789"
+  numbers:"0123456789"
+}
+function confirmLength(){
+  passwordLength=0;
+  while (passwordLength <8 || passwordLength > 128)
+  passwordLength=window.prompt("Pick a password length between 8 and 128 characters long.");
 }
 
+function generatePassword(){
+  var randomPassword = ""
+  var characterPool =""
+  confirmLength();
+  var confirmLower = window.confirm("Do you want lowercase?")
+  var confirmUpper = window.confirm("Do you want Uppercase?")
+  var confirmSpecial = window.confirm("Do you want special characters?")
+  var confirmNumbers = window.confirm("Do You want numbers?")
+  if (confirmLower) {
+    characterPool += passwordOptions.letters;
+  }
+  if (confirmUpper) {
+    characterPool += passwordOptions.letters.toUpperCase();
+  }
+  if (confirmSpecial){
+    characterPool += passwordOptions.special;
+  }
+  if (confirmNumbers){
+    characterPool += passwordOptions.numbers;
+  }
+  console.log(characterPool[0]);
+  for(var i = 0; i < passwordLength; i++){
+    randomPassword += characterPool[getRandomInt(characterPool.length)];
+  }
+  return randomPassword;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
